994eaf8f68f0897269089d89e407f4a86f93303e
Maddy <90936389+SethPlattner@users.noreply.github.com>
2023-01-21 11:42:56 -0800

    deleted everything

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,119 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonTrackedTarget;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
+/**
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
+ */
 public class Robot extends TimedRobot {
-	// get instances of all subsystems (w/ a subsystem manager for many subsystems)
+  private DifferentialDrive m_myRobot;
+  private Joystick stick;
 
-	private RobotContainer m_robotContainer;
+  private final CANSparkMax m_leftMotor_front = new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax m_leftMotor_back = new CANSparkMax(2, MotorType.kBrushless);
+  private final CANSparkMax  m_rightMotor_front = new CANSparkMax(3, MotorType.kBrushless);
+  private final CANSparkMax  m_rightMotor_back = new CANSparkMax(4, MotorType.kBrushless);
+  private final MotorControllerGroup left = new MotorControllerGroup(m_leftMotor_front,m_leftMotor_back);
+  private final MotorControllerGroup right = new MotorControllerGroup(m_rightMotor_front,m_rightMotor_back);
+  
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    right.setInverted(true);
 
-	private Command m_autonomousCommand;
-	private PhotonCamera camera;
-	private PIDController controller = new PIDController(Constants.DriveConstants.kP, Constants.DriveConstants.kI, 0);
-	private ProfiledPIDController profiledController = new ProfiledPIDController(Constants.DriveConstants.kP, 0, 0,  new TrapezoidProfile.Constraints(Constants.DriveConstants.Autos.maxVelocity, Constants.DriveConstants.Autos.maxAccel));
+    m_myRobot = new DifferentialDrive(left, right);
+    stick = new Joystick(0);
+  }
 
-	private final double camHeight = 1; //change
-	private final double targetHeight = 0.5; //change
-	private final double tagGoalDist = 1.5;
-
-	// leave constructor empty for now, unless something needs to be done every time
-	// robot is called
-	public Robot() {
-
-	}
-
-	@Override
-	public void robotInit() {
-		m_robotContainer = new RobotContainer();
-		// register subsystems into a list to minimize redundancy if logging
-		// inputs/outputs for all
-		camera = new PhotonCamera("USB2.0_PC_CAMERA");
-	}
-
-	@Override
-	public void robotPeriodic() {
-		// continue to run commands after initialization
-		CommandScheduler.getInstance().run();
-		// perform additional periodic calculations that don't fit inside the available
-		// commands
-		// e.g., pose calculations, outputting logs to smart dashboard, outputting
-		// telemetry
-		double forwardSpeed;
-		double rotation;
-		double range; 
-
-		var result = camera.getLatestResult();
-		boolean hasTargets = result.hasTargets();
-		if (hasTargets) {
-			
-			PhotonTrackedTarget target = result.getBestTarget();
-			double yaw = target.getYaw();
-			int targetID = target.getFiducialId();
-			System.out.println("got target id: " + targetID);
-
-			range  = PhotonUtils.calculateDistanceToTargetMeters(camHeight, targetHeight, 0, Units.degreesToRadians(result.getBestTarget().getPitch()));
-			rotation = -profiledController.calculate(yaw, 0);
-			forwardSpeed  = -controller.calculate(range, tagGoalDist);
-		} else {
-			//System.out.println("no targets found!");
-			forwardSpeed = 0;
-			rotation = 0;
-			range  = 0;
-		}
-
-		if (Math.abs(range-tagGoalDist) > 1 || Math.abs(rotation) > 5) {
-			m_robotContainer.getDrive().drive(rotation, forwardSpeed, 0.5, Constants.JoystickScaling.LINEAR, Constants.DriveStyle.ARCADE_TANK);
-		} else {
-			m_robotContainer.getDrive().tankDriveVolts(0, 0);
-		}
-	}
-
-	@Override
-	public void disabledInit() {
-		CommandScheduler.getInstance().cancelAll();
-		m_robotContainer.getDrive().setBreakStatus(true);
-	}
-
-	// e.g., turn off coprocessor stuff
-	@Override
-	public void disabledPeriodic() {
-	}
-
-	@Override
-	public void disabledExit() {
-		m_robotContainer.getDrive().setBreakStatus(false);
-	}
-
-	// initialize auto code (commands are periodically scheduled in robotPeriodic)
-	@Override
-	public void autonomousInit() {
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.schedule();
-		}
-	}
-
-	// schedule commands for what the robot should do after auto period ends
-	@Override
-	public void teleopInit() {
-
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
-
-		m_robotContainer.getDrive().setBreakStatus(false);
-	}
-
+  @Override
+  public void teleopPeriodic() {
+    m_myRobot.arcadeDrive(-stick.getY(), -stick.getX());
+  }
 }
