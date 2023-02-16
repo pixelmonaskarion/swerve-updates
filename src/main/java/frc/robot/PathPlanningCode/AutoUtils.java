495d5f9843c1c132d3b995b34dab73c308be166b
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-02-16 13:14:41 -0800

    other autos

--- src/main/java/frc/robot/PathPlanningCode/AutoUtils.java
+++ src/main/java/frc/robot/PathPlanningCode/AutoUtils.java
@@ -16,76 +16,103 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.RobotContainer;
+import frc.robot.Commands.VisionTurnCommand;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Subsystems.DriveSubsystem;
+
 
 public class AutoUtils {
     private SendableChooser<AutoModes> autoChooser = new SendableChooser<>();
+    private final TrajectoryConfig config = new TrajectoryConfig(
+      AutoConstants.kMaxSpeedMetersPerSecond,
+      AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+      .setKinematics(DriveConstants.kDriveKinematics);
+
     
     public AutoUtils() {
         autoChooser.setDefaultOption("Simple Drive", AutoModes.SIMPLE_DRIVE);
-
+        autoChooser.addOption("Simple Trajectory", AutoModes.SIMPLE_TRAJECTORY);
+        autoChooser.addOption("Auto Align Trajectory", AutoModes.AUTO_ALIGN_TRAJECTORY);
 
         SmartDashboard.putData("auto choices", autoChooser);
     }
 
-    public Command simpleCmdGrp(DriveSubsystem m_robotDrive) {
-        return new RunCommand(() -> m_robotDrive.drive(0, 0.8, 0, 0, false, true), m_robotDrive).withTimeout(5);
+
+    public Command simpleCmdGrp(RobotContainer container) {
+        return new RunCommand(() -> container.getDrive().drive(0, 0.8, 0, 0, false, true), container.getDrive()).withTimeout(5);
     }
 
 
-    public Command simpleTrajectoryCommand(DriveSubsystem m_robotDrive) {
-      TrajectoryConfig config = new TrajectoryConfig(
-          AutoConstants.kMaxSpeedMetersPerSecond,
-          AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-          .setKinematics(DriveConstants.kDriveKinematics);
-  
-      Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-          new Pose2d(0, 0, new Rotation2d(0)),
-          List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-          new Pose2d(3, 0, new Rotation2d(0)),
-          config);
-  
+    public Command simpleTrajectoryCommand(RobotContainer container, Trajectory trajectory) {
       var thetaController = new ProfiledPIDController(
           AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
       thetaController.enableContinuousInput(-Math.PI, Math.PI);
   
       SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-          exampleTrajectory,
-          m_robotDrive::getPose, // Functional interface to feed supplier
+          trajectory,
+          container.getDrive()::getPose, // Functional interface to feed supplier
           DriveConstants.kDriveKinematics,
   
           // Position controllers
           new PIDController(AutoConstants.kPXController, 0, 0),
           new PIDController(AutoConstants.kPYController, 0, 0),
           thetaController,
-          m_robotDrive::setModuleStates,
-          m_robotDrive);
+          container.getDrive()::setModuleStates,
+          container.getDrive());
 
-      m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+      container.getDrive().resetOdometry(trajectory.getInitialPose());
 
-      return swerveControllerCommand.andThen(() -> m_robotDrive.mainDrive(0, 0, 0));
+      return swerveControllerCommand.andThen(() -> container.getDrive().mainDrive(0, 0, 0));
     }
 
+    public Command trajectoryAutoAlign(RobotContainer container, Trajectory trajectory) {
+      return simpleTrajectoryCommand(container, trajectory)
+        .andThen(new VisionTurnCommand(container.getVision(), container.getDrive(), container.getController()));
+    }
+
+
+
+    public Trajectory simpleCurve() {
+      Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+          new Pose2d(0, 0, new Rotation2d(0)),
+          List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+          new Pose2d(3, 0, new Rotation2d(0)),
+          config);
+
+      return trajectory;
+    }
+
+    public Trajectory driveToScore() {
+      Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(new Translation2d(3, 0.5)),
+        new Pose2d(2, 2, new Rotation2d(45)),
+        config);
+
+      return trajectory;
+    }
   
 
 
-  public Command chooseAuto(DriveSubsystem m_robotDrive) {
+  public Command chooseAuto(RobotContainer container) {
     switch(autoChooser.getSelected()) {
       case SIMPLE_TRAJECTORY:
-        return simpleTrajectoryCommand(m_robotDrive);
+        return simpleTrajectoryCommand(container, simpleCurve());
+      case AUTO_ALIGN_TRAJECTORY:
+        return trajectoryAutoAlign(container, driveToScore());
       default:
-        return simpleCmdGrp(m_robotDrive);
+        return simpleCmdGrp(container);
     }
   }
+
 
   public SendableChooser<AutoModes> getChooser() {
     return autoChooser;
   }
     
 
-    private enum AutoModes {
-        SIMPLE_DRIVE, SIMPLE_TRAJECTORY
-    }
+  private enum AutoModes {
+        SIMPLE_DRIVE, SIMPLE_TRAJECTORY, AUTO_ALIGN_TRAJECTORY
+  }
 }
